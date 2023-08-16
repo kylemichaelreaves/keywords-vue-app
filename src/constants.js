41066cb81258345c8e775e2818be242e18f222ad
Kyle <kylereaves@gmail.com>
2023-08-16 15:59:54 -0400

    Deleting transpiled .js files.

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LAMBDA_DEV_URL = exports.API_GATEWAY_URL = void 0;
-var API_GATEWAY_URL = import.meta.env.VITE_APIGATEWAY_URL;
-exports.API_GATEWAY_URL = API_GATEWAY_URL;
-var LAMBDA_DEV_URL = 'http://127.0.0.1:3001';
-exports.LAMBDA_DEV_URL = LAMBDA_DEV_URL;
--- src/test/router.mock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockRouter = void 0;
-// src/spec/router.mock.ts
-var vue_router_1 = require("vue-router");
-exports.mockRouter = (0, vue_router_1.createRouter)({
-    history: (0, vue_router_1.createMemoryHistory)(),
-    routes: [],
-});
