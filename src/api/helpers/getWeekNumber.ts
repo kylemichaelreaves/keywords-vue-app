d316bfa171264c8fa27012e540eb6e69c3ea6fce
Kyle <kylereaves@gmail.com>
2023-12-28 22:01:53 -0500

    comments on getWeekNumber

--- src/api/helpers/getWeekNumber.ts
+++ src/api/helpers/getWeekNumber.ts
@@ -1,5 +1,13 @@
 export function getWeekNumber(date: Date): number {
+    // Get the first day of the year
     const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+
+    // Calculate the number of milliseconds between the given date and the first day of the year
+    // Then convert it to days by dividing by the number of milliseconds in a day (86400000)
     const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+
+    // Calculate the week number by adding the day of the week of the first day of the year (0-6)
+    // to the number of days passed in the year, adding 1 to make the first day of the year count as a whole day
+    // and then dividing by 7 (the number of days in a week), rounding up to the nearest whole number
     return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
 }
