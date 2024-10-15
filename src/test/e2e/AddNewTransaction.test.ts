1c3f027ee0c84cb40dbabdb0097107d8dd31ece6
Kyle <kylereaves@gmail.com>
2024-10-15 18:34:28 -0400

    finding the button by role and not by text

--- src/test/e2e/AddNewTransaction.test.ts
+++ src/test/e2e/AddNewTransaction.test.ts
@@ -3,21 +3,21 @@ import {test, expect} from '@playwright/test';
 
 test('clicking the button opens the form', async ({page}) => {
     await page.goto('/budget-visualizer');
-    await page.getByText('budget-visualizer').click();
+    await page.getByRole('tab', { name: 'budget-visualizer' }).click();
     await page.getByRole('button', {name: 'Add New Transaction'}).click();
     await expect(page.getByText('Add New TransactionCreditDebitDateDescriptionMemoAmount DebitAmount Credit')).toBeVisible();
 })
 
 test('the default type is Debit and the Amount Credit field is disabled', async ({page}) => {
     await page.goto('/budget-visualizer');
-    await page.getByText('budget-visualizer').click();
+    await page.getByRole('tab', { name: 'budget-visualizer' }).click();
     await page.getByRole('button', {name: 'Add New Transaction'}).click();
     await expect(page.getByLabel('Amount Credit')).toBeDisabled();
 })
 
 test('clicking the Credit radio button enables the Amount Credit input and the Amount Debit is disabled', async ({page}) => {
     await page.goto('/budget-visualizer');
-    await page.getByText('budget-visualizer').click();
+    await page.getByRole('tab', { name: 'budget-visualizer' }).click();
     await page.getByRole('button', {name: 'Add New Transaction'}).click();
     await page.locator('div').filter({ hasText: /^CreditDebit$/ }).locator('div').click();
     await expect(page.getByLabel('Amount Credit')).toBeEnabled();
@@ -26,7 +26,7 @@ test('clicking the Credit radio button enables the Amount Credit input and the A
 
 test('clicking the Close closes the form', async ({page}) => {
     await page.goto('/budget-visualizer');
-    await page.getByText('budget-visualizer').click();
+    await page.getByRole('tab', { name: 'budget-visualizer' }).click();
     await page.getByRole('button', {name: 'Add New Transaction'}).click();
     await page.getByLabel('Close this dialog').click();
     await expect(page.getByText('Add New TransactionCreditDebitDateDescriptionMemoAmount DebitAmount Credit')).not.toBeVisible();
