ba367f23f9756080a4f38ac33361f7bc682a63ab
Kyle <kylereaves@gmail.com>
2024-10-15 20:16:28 -0400

    mocking the data like a civilized engineer, rather than a slovenly, slothlike creature just winging it

--- src/test/e2e/TransactionsTable.test.ts
+++ src/test/e2e/TransactionsTable.test.ts
@@ -1,8 +1,18 @@
 // import {test, expect} from '@playwright/test';
 import {test, expect} from '@test/e2e/fixtures/PageFixture';
 import {TransactionsPage} from "@test/e2e/pages/TransactionsPage";
+import {transactionsMock} from "@mocks/transaction";
 
 test('clicking the Transactions icon on the menu NavBar opens the TransactionsTable', async ({page}) => {
+
+
+    await page.route('**/transactions/get-transactions?*', async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify(transactionsMock),
+        });
+    });
 
     const transactionsPage = new TransactionsPage(page);
 
@@ -15,6 +25,6 @@ test('clicking the Transactions icon on the menu NavBar opens the TransactionsTa
     await expect(transactionsPage.monthSelect).toBeVisible();
     await expect(transactionsPage.yearSelect).toBeVisible();
 
-    await expect(transactionsPage.transactionsTable).toBeVisible({ timeout: 5000 });
+    await expect(transactionsPage.transactionsTable).toBeVisible({timeout: 5000});
 
 });
