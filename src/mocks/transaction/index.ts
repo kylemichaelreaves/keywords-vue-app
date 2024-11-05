a1e554abeb7a7d0ab13d4881d4db00de5a010398
Kyle <kylereaves@gmail.com>
2024-11-05 14:34:06 -0500

    mocking the interval data

--- src/mocks/transaction/index.ts
+++ src/mocks/transaction/index.ts
@@ -43,16 +43,120 @@ export function generateTransactions(length: number): Transaction[] {
     const result: Transaction[] = [];
     const startDate = DateTime.fromISO(transactionsMock[0].date);
 
-    for (let i = 0; i < length; i++) {
+    for (let i = 1; i < length; i++) {
         const baseTransaction = transactionsMock[i % transactionsMock.length];
         const newTransaction = {
             ...baseTransaction,
-            Date: startDate.plus({ days: i }).toISO(), // Increment the date by `i` days
+            Date: startDate.plus({days: i}).toISO(), // Increment the date by `i` days
             "Transaction Number": `XXXXXXX${String(i).padStart(4, '0')}XXXX`, // Optional: unique Transaction Number
             Memo: `${baseTransaction.memo} ${i + 1}` // Optional: add index to Memo for uniqueness
         };
         result.push(newTransaction);
     }
+    return result;
+}
+
+interface IntervalData {
+    date: string;
+    total_amount_debit: number;
+}
+
+export const intervalsMock =
+    [
+        {
+            "date": "2024-10-07T00:00:00.000Z",
+            "total_amount_debit": -614.4100000000001
+        },
+        {
+            "date": "2024-10-08T00:00:00.000Z",
+            "total_amount_debit": -598.49
+        },
+        {
+            "date": "2024-10-09T00:00:00.000Z",
+            "total_amount_debit": -99.88000000000001
+        },
+        {
+            "date": "2024-10-10T00:00:00.000Z",
+            "total_amount_debit": -632.75
+        },
+        {
+            "date": "2024-10-11T00:00:00.000Z",
+            "total_amount_debit": -130.62
+        },
+        {
+            "date": "2024-10-15T00:00:00.000Z",
+            "total_amount_debit": -351.43000000000006
+        },
+        {
+            "date": "2024-10-16T00:00:00.000Z",
+            "total_amount_debit": -87.59
+        },
+        {
+            "date": "2024-10-17T00:00:00.000Z",
+            "total_amount_debit": -109.37
+        },
+        {
+            "date": "2024-10-18T00:00:00.000Z",
+            "total_amount_debit": -61.91
+        },
+        {
+            "date": "2024-10-21T00:00:00.000Z",
+            "total_amount_debit": -345.59000000000003
+        },
+        {
+            "date": "2024-10-22T00:00:00.000Z",
+            "total_amount_debit": -208.28000000000003
+        },
+        {
+            "date": "2024-10-23T00:00:00.000Z",
+            "total_amount_debit": -79.69
+        },
+        {
+            "date": "2024-10-24T00:00:00.000Z",
+            "total_amount_debit": -104.13
+        },
+        {
+            "date": "2024-10-25T00:00:00.000Z",
+            "total_amount_debit": -135.29
+        },
+        {
+            "date": "2024-10-28T00:00:00.000Z",
+            "total_amount_debit": -325.57
+        },
+        {
+            "date": "2024-10-29T00:00:00.000Z",
+            "total_amount_debit": -52.17
+        },
+        {
+            "date": "2024-10-30T00:00:00.000Z",
+            "total_amount_debit": -44.97
+        },
+        {
+            "date": "2024-10-31T00:00:00.000Z",
+            "total_amount_debit": -216.09
+        }
+    ]
+
+export function generateDailyInterval(length: number): IntervalData[] {
+    const result: IntervalData[] = [];
+    const startDate = DateTime.fromISO(transactionsMock[0]["Date"]);
+
+    for (let i = 1; i < length; i++) {
+        const baseTransaction = transactionsMock[i % transactionsMock.length];
+
+        // Increment the date by `i` days
+        const date = startDate.plus({days: i}).toISODate();
+
+        // Ensure the total_amount_debit is a negative integer
+        const amountDebit = baseTransaction["Amount Debit"];
+        const total_amount_debit = Math.floor(amountDebit); // Convert to integer
+
+        result.push(<IntervalData>{
+            date: date,
+            total_amount_debit: total_amount_debit,
+        });
+    }
+
     return result;
 }
 
--- src/test/e2e/TransactionsTable.test.ts
+++ src/test/e2e/TransactionsTable.test.ts
@@ -1,7 +1,7 @@
 // import {test, expect} from '@playwright/test';
 import {test, expect} from '@test/e2e/fixtures/PageFixture';
 import {TransactionsPage} from "@test/e2e/pages/TransactionsPage";
-import {generateTransactions} from "@mocks/transaction";
+import { generateTransactions, intervalsMock} from "@mocks/transaction";
 
 test('clicking the Transactions icon on the menu NavBar opens the TransactionsTable', async ({page}) => {
     await page.route('**/transactions/get-transactions?*', async (route) => {
@@ -9,6 +9,14 @@ test('clicking the Transactions icon on the menu NavBar opens the TransactionsTa
             status: 200,
             contentType: 'application/json',
             body: JSON.stringify(generateTransactions(200)),
+        });
+    });
+
+    await page.route('**/transactions/get-daily-total-amount-debit?*', async (route) => {
+        await route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify(intervalsMock),
         });
     });
 
@@ -22,7 +30,7 @@ test('clicking the Transactions icon on the menu NavBar opens the TransactionsTa
     await expect(transactionsPage.monthSelect).toBeVisible();
     await expect(transactionsPage.yearSelect).toBeVisible();
     await expect(transactionsPage.memoSelect).toBeVisible();
-    await expect(transactionsPage.intervalLineChart).toBeVisible({timeout: 10000});
+    await expect(transactionsPage.intervalLineChart).toBeVisible();
     await expect(transactionsPage.intervalTypeSelect).toBeVisible();
     await expect(transactionsPage.intervalNumberInput).toBeVisible();
     await expect(transactionsPage.transactionsTablePagination).toBeVisible();
